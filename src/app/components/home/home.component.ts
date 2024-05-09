4ce5f7639452df337bb0ca922915c0a30b5fe6af
Jasmin Cordeiro <playgame0159@gmail.com>
2024-05-09 19:53:13 -0300

    Some updates

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,11 +4,15 @@ import { FooterComponent } from '../footer/footer.component';
 import { NgOptimizedImage } from '@angular/common';
 import { CarouselComponent } from '../carousel/carousel.component';
 import { ItemsCarouselComponent } from '../items-carousel/items-carousel.component';
+import {MatButtonModule} from '@angular/material/button';
+
+
+
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,FooterComponent,NgOptimizedImage,CarouselComponent,ItemsCarouselComponent],
+  imports: [HeaderComponent,FooterComponent,NgOptimizedImage,CarouselComponent,ItemsCarouselComponent,MatButtonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
